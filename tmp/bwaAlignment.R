45ef9308db6af182ec075e4931ffdee2995318a1
msg <mb2subi@gmail.com>
2020-04-08 16:46:59 +0200

    upload bwaAlignment

--- tmp/bwaAlignment.R
+++ tmp/bwaAlignment.R
@@ -7,11 +7,13 @@ args <- commandArgs(trailingOnly = TRUE)
 
 fastq <- args[1]
 out_path <- args[2]
+type_input_file <- args[3]
 
 bwaAlignment(input_file = fastq,
              ref = ref,
              out_path = out_path,
-             threads = threads)
+             threads = threads,
+             type_input_file = type_input_file)
 
 
 
