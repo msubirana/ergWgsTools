9a39210e0bcd8bbbb90a481fe06366cd49c2ce47
msg <mb2subi@gmail.com>
2020-05-26 18:07:59 +0200

    added purple

--- tmp/purple_full_host.R
+++ tmp/purple_full_host.R
@@ -1,11 +1,12 @@
 devtools::load_all('/imppc/labs/lplab/share/marc/repos/ergWgsTools')
-bam_path <- getwd()
-out_path <- getwd()
+# bam_path <- getwd()
+# bams <- list.files(bam_path,
+#                    pattern = "\\_BL.bam$",
+#                    full.names = T)
 
-bams <- list.files(bam_path,
-                   pattern = "\\_BL.bam$",
-                   full.names = T)
-cores <- 24
+bams <- c('/home/labs/lplab/msubirana/media/drive6/insulinomas/processed/hg38/bam/bwa/NET-21_BL.bam')
+out_path <- '/home/labs/lplab/msubirana/media/drive6/insulinomas/processed/hg38/purple'
+cores <- 2
 
 for(bam in bams){
 
