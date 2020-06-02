414330b7cefbf9d8847799408d8243194ec37117
msg <mb2subi@gmail.com>
2020-06-02 15:39:48 +0200

    added merge_pass_strelka

--- tmp/merge_pass_strelka_host.R
+++ tmp/merge_pass_strelka_host.R
@@ -1,7 +1,7 @@
 raw_strelka <- '/imppc/labs/lplab/share/marc/insulinomas/processed/hg38/vcf/strelka2/raw'
-list.files(raw_strelka,
-           full.names = T,
-           pattern = '_out_strelka2$')
+strelka_paths <- list.files(raw_strelka,
+                            full.names = T,
+                            pattern = '_out_strelka2$')
 
 for(strelka_path in strelka_paths){
   merge_pass_strelka(strelka_path)
