26be44d174b90aee67558815583103d05214fbe1
msg <mb2subi@gmail.com>
2020-06-02 15:40:23 +0200

    added merge_pass_strelka

--- tmp/merge_pass_strelka_host.R
+++ tmp/merge_pass_strelka_host.R
@@ -1,3 +1,4 @@
+devtools::load_all('/imppc/labs/lplab/share/marc/repos/ergWgsTools')
 raw_strelka <- '/imppc/labs/lplab/share/marc/insulinomas/processed/hg38/vcf/strelka2/raw'
 strelka_paths <- list.files(raw_strelka,
                             full.names = T,
